49677ac76182945d84a9a2ab748a1d348857579c
saugatjung <64913290+Saugat12345@users.noreply.github.com>
2022-02-27 15:22:26 +0545

    admin panels changed

--- admins/admin.py
+++ admins/admin.py
@@ -1,3 +1,42 @@
-from django.contrib import admin
 
-# Register your models here.
+<a
+  class="btn btn-primary m-2"
+  data-bs-toggle="offcanvas"
+  href="#offcanvasExample"
+  role="button"
+  aria-controls="offcanvasExample"
+>
+  Menu
+</a>
+<div
+  class="offcanvas offcanvas-start"
+  tabindex="-1"
+  id="offcanvasExample"
+  aria-labelledby="offcanvasExampleLabel"
+>
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="offcanvasExampleLabel">Admin Dashboard</h5>
+    <button
+      type="button"
+      class="btn-close text-reset"
+      data-bs-dismiss="offcanvas"
+      aria-label="Close"
+    ></button>
+  </div>
+  <div class="offcanvas-body">
+    <ul class="list-unstyled">
+      <li><a class="dropdown-item" href=">Admin Dashboard</a></li>
+      <li><a class="dropdown-item" href="">Products</a></li>
+
+      <li><a class="dropdown-item" href="">USER</a></li>
+      <li><a class="dropdown-item" href="">ADMINS</a></li>
+
+      <li><a class="dropdown-item" href="">Setting</a></li>
+      <li>
+        <a class="dropdown-item" href="">Change password</a>
+      </li>
+      <li><a class="dropdown-item" href="">LOGOUT</a></li>
+    </ul>
+  </div>
+</div>
+
